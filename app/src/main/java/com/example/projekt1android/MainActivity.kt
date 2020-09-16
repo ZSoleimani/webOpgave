ff6170efbf9be3d214b84543c8dc3556c8380c91
Zahra <s170422@student.dtu.dk>
2020-09-16 12:20:38 +0200

    firste commit

---
+++ app/src/main/java/com/example/projekt1android/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.projekt1android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        besked.setOnClickListener{
+            title
+        }
+    }
+}
